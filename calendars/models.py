7dd024afdd655512fc52fa9c524b9c89c1f5fa5a
ali <weplutus.1@gmail.com>
2021-05-29 13:35:53 +0300

    recurence logic

--- calendars/models.py
+++ calendars/models.py
@@ -1,34 +1,67 @@
+import inspect
 from django.db import models
 from safedelete.models import SafeDeleteModel, NO_DELETE
 from users.models import User
 from django.conf import settings
 from multiselectfield import MultiSelectField
-
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+from django import forms
 
 REC = (
-    ('day', 'Every day.'),
-    ('month', 'Every month.'),
-    ('year', 'Every year.'),
-    ('sunday', 'Every sunday.'),
-    ('monday', 'Every monday.'),
-    ('tuesday', 'Every tuesday.'),
-    ('wednesday', 'Every wednesday.'),
-    ('thursday', 'Every thursday.'),
-    ('friday', 'Every friday.'),
-    ('saturday', 'Every saturday.'),
+    ('0 G day', 'Every day.'),
+    ('0 month', 'Every month.'),
+    ('0 year', 'Every year.'),
+    ('1 sunday', 'Every sunday.'),
+    ('1 monday', 'Every monday.'),
+    ('1 tuesday', 'Every tuesday.'),
+    ('1 wednesday', 'Every wednesday.'),
+    ('1 thursday', 'Every thursday.'),
+    ('1 friday', 'Every friday.'),
+    ('1 saturday', 'Every saturday.'),
+
+    #  TODO recurnce on spesifc months
+    # ('2 aprile', 'Every aprile.'),
+    # ('2 june', 'Every june.'),
+    # ('2 julay', 'Every julay.'),
+    # ('2 aprile', 'Every aprile.'),
+    # ('2 aprile', 'Every aprile.'),
+    # ('2 aprile', 'Every aprile.'),
+    # ('2 aprile', 'Every aprile.'),
+    # ('2 julay', 'Every julay.'),
+    # ('2 aprile', 'Every aprile.'),
+    # ('2 aprile', 'Every aprile.'),
+    # ('2 aprile', 'Every aprile.'),
+    # ('2 aprile', 'Every aprile.'),
 
 )
-
-# def validate_chose(value):
-#     print('======================')
-#     print(value)
-#     print('======================')
 
 
 class DateType(SafeDeleteModel):
     name = models.CharField(max_length=30, unique=False)
     description = models.CharField(
         max_length=30, unique=False, null=True, blank=True)
+
+
+def myfunction(self, *args, **keyarg):
+    if str(self).count('0') > 1:
+        raise ValidationError(
+            _("You should choose one, either every day or every month or every year"),)
+
+    if str(self).count('G') >= 1 and str(self).count('1') >= 1:
+        days = list(filter(lambda k: '1' in k, self))
+        fD = []
+        for day in days:
+            fD.append(day.replace('1 ', '').title())
+        raise ValidationError(
+            _("If it reapeate every day it will repeated on "+str(fD)+" as well"),)
+
+
+class Rec(MultiSelectField):
+    def _choices_is_value(self, *args, **keyarg):
+        print('_choices_is_value======================')
+        self.validators = [myfunction]
+        return super()._choices_is_value(*args, **keyarg)
 
 
 class Date(SafeDeleteModel):
@@ -42,5 +75,4 @@ class Date(SafeDeleteModel):
     created_by = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     users = models.ManyToManyField(User, related_name='date_with', blank=True)
-    recurrence = MultiSelectField(
-        choices=REC, default='none')
+    recurrence = Rec(choices=REC, blank=True)
